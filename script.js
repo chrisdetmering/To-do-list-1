c57e28d7c59e9800252f8020691d9e0feaa6fd18
f3n1xfit <80013607+f3n1xfit@users.noreply.github.com>
2021-04-02 15:15:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 
 let todoItems = [];
 
-function createToDoList(todo) {
-  const list = document.querySelector("#userslist")
 
-  const listitem = document.createElement("li")
-  listitem.setAttribute("class"), 
-}
+
+

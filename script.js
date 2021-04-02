c12eb864269773f32383cc77976c20e4d9f9d6a3
f3n1xfit <80013607+f3n1xfit@users.noreply.github.com>
2021-04-02 15:48:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 'use strict';
 
-let todoItems = [];
+function addtoList() {
+  let myList = document.getElementById("userslist");
+  let userinput = document.getElementById("userinput");
+  var li = document.createElement("li");
 
+  li.setAttribute('id', userinput.nodeValue);
+  li.appendChild(document.createTextNode(userinput.nodeValue));
+}
 
-
-
+addtoList();

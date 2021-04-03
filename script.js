9d70e814cee75cb2f2120b34a0976542585881d6
f3n1xfit <80013607+f3n1xfit@users.noreply.github.com>
2021-04-03 08:39:31 -0700

    finished the add to do part

--- script.js
+++ script.js
@@ -8,19 +8,26 @@ function addtoList(e) {
   let myList = document.getElementById("userslist");
   let userinput = document.getElementById("userInput");
   let li = document.createElement("li");
+
   let checkbox = document.createElement('input');
   checkbox.type = "checkbox";
-  checkbox.value = 1;
+  checkbox.checked = false;
   checkbox.name = userinput.value;
   checkbox.id = "box"
 
+  let deleteButton = document.createElement("button");
+  deleteButton.id = "deleteButton";
+  deleteButton.innerText = "X";
+  deleteButton.name = userinput.value;
 
   li.setAttribute('id', userinput.value);
   li.appendChild(document.createTextNode(userinput.value));
 
+  li.appendChild(deleteButton);
   li.appendChild(checkbox);
   myList.appendChild(li);
-
 }
+
+
 
 

c21d09349a6bbcd49bab78fd4c0b6e3acad9b9b0
f3n1xfit <80013607+f3n1xfit@users.noreply.github.com>
2021-04-02 22:09:49 -0700

    progress

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 'use strict';
-const button = getElementById("sbmt_btn")
-button.addEventListener("click", addtoList);
+const button = document.getElementById('sbmt_btn')
+button.addEventListener('click', addtoList);
 
 function addtoList(e) {
   e.preventDefault();
 
   let myList = document.getElementById("userslist");
-  let userinput = document.getElementById("userinput");
-  var li = document.createElement("li");
+  let userinput = document.getElementById("userInput");
+  let li = document.createElement("li");
+  let checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.value = 1;
+  checkbox.name = userinput.value;
+  checkbox.id = "box"
+
 
   li.setAttribute('id', userinput.value);
   li.appendChild(document.createTextNode(userinput.value));
 
+  li.appendChild(checkbox);
   myList.appendChild(li);
+
 }
+
 

102fa884fd84fc65744d4badd3d572b9c40b589f
f3n1xfit <80013607+f3n1xfit@users.noreply.github.com>
2021-04-02 20:46:29 -0700

    hi

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 'use strict';
+const button = getElementById("sbmt_btn")
+button.addEventListener("click", addtoList);
 
-function addtoList() {
+function addtoList(e) {
+  e.preventDefault();
+
   let myList = document.getElementById("userslist");
   let userinput = document.getElementById("userinput");
   var li = document.createElement("li");
 
-  li.setAttribute('id', userinput.nodeValue);
-  li.appendChild(document.createTextNode(userinput.nodeValue));
+  li.setAttribute('id', userinput.value);
+  li.appendChild(document.createTextNode(userinput.value));
+
+  myList.appendChild(li);
 }
 
-addtoList();

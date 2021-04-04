b872f9f69abc78ae15da6dd78ef5c7d6bf98ea2a
f3n1xfit <80013607+f3n1xfit@users.noreply.github.com>
2021-04-04 08:58:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,36 +2,41 @@
 const button = document.getElementById('sbmt_btn')
 button.addEventListener('click', addtoList);
 
+
 function addtoList(e) {
   e.preventDefault();
 
   const myList = document.getElementById("userslist");
   const userinput = document.getElementById("userInput");
-  const li = document.createElement("li");
+  if (userinput.value == '') {
+    alert('Please enter your to-do')
+  } else {
+    const li = document.createElement("li");
 
-  const checkbox = document.createElement('input');
-  checkbox.type = "checkbox";
-  checkbox.checked = false;
-  checkbox.name = userinput.value;
-  checkbox.id = "box";
-  checkbox.addEventListener('change', function () {
-    li.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
-  });
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.checked = false;
+    checkbox.name = userinput.value;
+    checkbox.id = "box";
+    checkbox.addEventListener('change', function () {
+      li.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
+    });
 
-  const deleteButton = document.createElement("button");
-  deleteButton.id = "deleteButton";
-  deleteButton.innerText = "X";
-  deleteButton.name = userinput.value;
-  deleteButton.addEventListener('click', function () {
-    li.remove();
-  });
+    const deleteButton = document.createElement("button");
+    deleteButton.id = "deleteButton";
+    deleteButton.innerText = "X";
+    deleteButton.name = userinput.value;
+    deleteButton.addEventListener('click', function () {
+      li.remove();
+    });
 
-  li.setAttribute('id', userinput.value);
-  li.appendChild(document.createTextNode(userinput.value));
+    li.setAttribute('id', userinput.value);
+    li.appendChild(document.createTextNode(userinput.value));
 
-  li.appendChild(deleteButton);
-  li.appendChild(checkbox);
-  myList.appendChild(li);
+    li.appendChild(deleteButton);
+    li.appendChild(checkbox);
+    myList.appendChild(li);
+  }
 }
 
 
